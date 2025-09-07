282734504fa3ae47219e13e519d748e40c08bdec
Olivia Grace <ograce2@my.bcit.ca>
2025-09-06 18:48:25 -0700

    Removed test code and commented code and finished docstrings

--- COMP4537/labs/0/js/script.js
+++ COMP4537/labs/0/js/script.js
@@ -1,7 +1,3 @@
-
-//import * as userMessages from '../lang/messages/en/user.js';
-
-
 /**
  * The Game class manages the overall game state and logic. It creates the necessary
  * elements for the user to play the game, as well as methods to manage the flow of the
@@ -20,105 +16,41 @@ class Game{
     static MAX_NUM = 7;
 
     /**
-     * Creates a new Game instance. Initializes the buttons array, the input form, and 
-     * the nextButton attribute.
+     * Creates a new Game instance. Initializes the buttons array, the input form, num,
+     * nextButton, display, and buttonDiv attributes.
      */
     constructor(){
         this.buttons = [];
-        //this.goButton = null;
         this.num = 0;
-
+        this.nextButton = 1;
         this.display = new Display();
-
         this.inputID = "gameInput";
-
-
         this.buttonDiv = Display.getButtonDisplayDiv();
-
         this.inputForm = new InputForm(Game.MIN_NUM, Game.MAX_NUM, "gameInput", "goButton", this.start.bind(this));
         this.display.addElementToDiv(this.inputForm.getForm());
-
-        
-        //this.buttonDiv.class = "initButtonDiv";
         document.body.appendChild(this.buttonDiv);
-        
-        //this.width = 0;
-        //this.height = 0;
-
-        this.nextButton = 1;
-
     }
-
+ 
+    /**
+     * Resets the game by setting nextButton to 1, num to 0, and removing the existing buttons from the window.
+     */
     resetGame(){
         this.nextButton = 1;
+        this.num = 0;
         this.removeButtons();
-        this.display.displayMessage(GAME_IN_PROGRESS);
     }
 
-    // Scrambles the buttons on the screen
-
     /**
-     * Scrambles the buttons on the screen to random locations this.num number of times with a delay between
-     * each scrabmble.
+     * Scrambles the buttons on the screen to random locations num number of times with a 2 second 
+     * delay between each scrabmble. Once the buttons have been scrambled, the button orders are hidden
+     * and the buttons are enabled.
      */
     scrambleButtons(scramblesLeft){
-        console.log("Scrambling buttons...");
-
-        // for (let i = 0; i < this.buttons.length; i++){
-        //     console.log("scrambling button " + (i + 1));
-        //     this.buttons[i].hideText();
-        //     // let left = Display.getRandomWidth() + "px";
-        //     // let top = Display.getRandomHeight() + "px";
-        //     let height = this.buttons[i].btn.offsetHeight;
-        //     let width = this.buttons[i].btn.offsetWidth;
-        //     let left = Display.getRandomLocationHorizontal(width) + "px";
-        //     let top = Display.getRandomLocationVertical(height) + "px";
-        //     console.log("Screen size: " + screen.width + ", " + screen.height);
-        //     console.log("New position: " + left + ", " + top);
-        //     this.buttons[i].setLocation(left, top);
-        // }
-    //     if (scramblesLeft > 1){{
-    //         this.buttons.forEach(button => function (){
-    //         let height = button.btn.offsetHeight;
-    //         let width = button.btn.offsetWidth;
-    //         let left = Display.getRandomLocationHorizontal(width) + "px";
-    //         let top = Display.getRandomLocationVertical(height) + "px";
-    //         console.log("Screen size: " + screen.width + ", " + screen.height);
-    //         console.log("New position: " + left + ", " + top);
-    //         button.setLocation(left, top);
-    //     }
-    // );
-        
-
-    //     }
-
-        // if (scramblesLeft > 1){
-        //     console.log("Scrambles left: " + scramblesLeft);
-        //     this.buttons.forEach(button => {
-        //         let height = button.btn.offsetHeight;
-        //         let width = button.btn.offsetWidth; 
-        //         let left = Display.getRandomLocationHorizontal(width) + "px";
-        //         let top = Display.getRandomLocationVertical(height) + "px";
-        //         console.log("Screen size: " + screen.width + ", " + screen.height);
-        //         console.log("New position: " + left + ", " + top);
-        //         button.setLocation(left, top);
-        //     });
-        //     setTimeout(this.scrambleButtons.bind(this, scramblesLeft - 1), 2000);
-        // } else{
-        //     this.hideButtonOrder();
-        //     this.enableButtons();
-
-        //     return;
-        // }
-
-        console.log("Scrambles left: " + scramblesLeft);
         this.buttons.forEach(button => {
             let height = button.btn.offsetHeight;
             let width = button.btn.offsetWidth; 
             let left = this.display.getRandomLocationHorizontal(width) + "px";
             let top = this.display.getRandomLocationVertical(height) + "px";
-            console.log("Screen size: " + screen.width + ", " + screen.height);
-            console.log("New position: " + left + ", " + top);
             button.setLocation(left, top);
         });
 
@@ -130,14 +62,7 @@ class Game{
             this.inputForm.enableFormSubmission();
             return;
         }
-        
-
-        // using width and height, place all buttons randomly on screen
     }
-
-    // getInputValue(){
-    //     return this.inputForm.getInputValue();
-    // }
 
     /**
      * Handler attached to a button to check if the user clicked the correct button.
@@ -148,21 +73,17 @@ class Game{
      * @param {*} event 
      */
     checkUserGuess(event){
-        // TO DO --> still need to display messages to user
         let buttonID = event.srcElement.id;
         let order = buttonID.substring(buttonID.length - 1);
         if (order == this.nextButton && this.nextButton < this.num){
-            console.log("Correct! You clicked button " + this.nextButton);
             document.getElementById(buttonID).innerHTML = this.nextButton;
             this.nextButton++;
         } else if (order == this.nextButton && this.nextButton == this.num){
             document.getElementById(buttonID).innerHTML = this.nextButton;
-            console.log("You win! You clicked all the buttons in the correct order.");
-            this.display.displayMessage(WIN);
+            Display.displayMessage(WIN);
             this.revealAnswer();
         } else{
-            console.log("Game over! You clicked the wrong button.");
-            this.display.displayMessage(LOSE);
+            Display.displayMessage(LOSE);
             this.revealAnswer();
         }
     }
@@ -171,7 +92,6 @@ class Game{
      * Removes all buttons from the page and clears the buttons array.
      */
     removeButtons(){
-
         if (this.buttons.length == 0){
             return;
         }
@@ -181,7 +101,6 @@ class Game{
         }
 
         this.buttons = [];
-
     }
 
     /**
@@ -191,25 +110,18 @@ class Game{
      * @param {*} num 
      */
     initializeButtons(){
-
         for (let i = 0; i < this.num; i++){
             let btn = new ScrambleButton(i + 1, "button" + (i + 1), Display.getRandomColour(), "10em", "5em");
             btn.btn.addEventListener("click", this.checkUserGuess.bind(this));
             this.buttons.push(btn);
             this.buttonDiv.appendChild(btn.btn);
-            console.log("Button " + (i + 1) + " created.");
         }
-
-        //document.body.appendChild(this.buttonDiv);
-
-        
     }
 
     /**
      * Enables all buttons in the buttons array by calling their enableButton method.
      */
     enableButtons(){
-
         for (let k = 0; k < this.buttons.length; k++){
             this.buttons[k].enableButton();
         }
@@ -224,71 +136,28 @@ class Game{
         }
     }
 
-    // Starts a new game
+    /**
+     * Starts a new game by resetting the game, initializing the buttons, and calling the scramble
+     * method.
+     */
     start(){
-        console.log("Game started with " + document.getElementById(this.inputID).value + " buttons.");
         this.resetGame();
         this.num = document.getElementById(this.inputID).value;
-
-        // get rid of buttons from the page
-        //this.removeButtons();
-
         this.initializeButtons();
-
-
-
-        // for (let i = 0; i < this.num; i++){
-        //     let btn = new ScrambleButton(i + 1, "button" + (i + 1), Display.getRandomColour(), "10em", "5em");
-        //     btn.btn.addEventListener("click", this.checkUserGuess.bind(this));
-        //     this.buttons.push(btn);
-        //     document.body.appendChild(btn.btn);
-        //     Display.setButtonLocation(this.num, this.buttons[i]);
-        //     console.log("Button " + (i + 1) + " created.");
-        // }
-
-        // setTimeout(this.scrambleButtons.bind(this), this.num * 1000); 
-        // console.log("SCRAMBLE ONCE");
-
-        // for (let i = 0; i < this.num - 1; i++){
-        //     console.log("SCRAMBLE AGAIN");
-        //     setTimeout(this.scrambleButtons.bind(this), 2000);
-
-        // }
-
         this.inputForm.disableFormSubmission();
         setTimeout(this.scrambleButtons.bind(this, this.num), this.num*1000);
-
-
-        // get input
-        // set num
-        // create buttons
-        // scramble buttons num amount of times
     }
-
-    // beginScramble(){
-    //     setInterval(this.scrambleButtons.bind(this), this.num * 1000); 
-    //     console.log("PAUSE: 1");
-    //     for (let j = 0; j < this.num - 1; j++){
-    //         console.log("PAUSE: " + (j + 2));
-    //         setInterval(this.scrambleButtons.bind(this), 2000);
-
-    //     }
-    //     return;
-    // }
 
     /**
      * Reveals the answers on the buttons after the games is over.
      */
     revealAnswer(){
-        // reveal the answers to the buttons
         this.buttons.forEach(button => button.showText());
         this.buttons.forEach(button => button.disableButton());
         this.buttons.forEach(button => button.makeStatic());
     }
-
-
-
 }
+
 
 /**
  * The InputForm class creates a form with a number input, label, and button for
@@ -318,11 +187,12 @@ class InputForm{
         this.input.value = minNum;
         this.input.step = this.stepValue;
         this.input.id = inputId;
+        this.input.className = "spacing";
 
         this.label = document.createElement("label");
         this.label.for = this.id;
         this.label.innerHTML = "Enter a number between " + minNum + " and " + maxNum + ": ";
-        this.btn = new Button("Go", buttonID, "lightblue", "50px", "25px", buttonFunction);
+        this.btn = new Button("Go", buttonID, "lightblue", "5em", "2em", buttonFunction);
 
         this.form.appendChild(this.label);
         this.form.appendChild(this.input);
@@ -330,29 +200,35 @@ class InputForm{
 
         this.form.addEventListener("submit", function(event){
             event.preventDefault();
-            let num = document.getElementById(inputId).value;
-            console.log("Form submitted with value: " + num);
             buttonFunction();
         });
         
-
         document.body.appendChild(this.form);
-
     }
 
+    /**
+     * Returns the form element of the InputForm.
+     * @returns the HTML form element of the InputForm
+     */
     getForm(){
         return this.form;
     }
 
+    /**
+     * Disables the Go button of the form.
+     */
     disableFormSubmission(){
         this.btn.disableButton();
     }
 
+    /**
+     * Enables the Go button of the form.
+     */
     enableFormSubmission(){
         this.btn.enableButton();
     }
-
 }
+
 
 /**
  * The Button class creates a button element with specified text, id, color, width, and height.
@@ -373,8 +249,7 @@ class Button{
         this.btn.id = id;
         this.btn.style.backgroundColor = colur;
         this.btn.style.width = width;
-        this.btn.style.height = height;
-        //this.btn.style.position = "absolute";
+        this.btn.style.height = height; 
     }
 
     /**
@@ -413,6 +288,9 @@ class ScrambleButton extends Button{
         this.btn.disabled = true;
     }
 
+    /**
+     * Makes the button position static.
+     */
     makeStatic(){
         this.btn.style.position = "static";
     }
@@ -429,14 +307,6 @@ class ScrambleButton extends Button{
      */
     showText(){
         this.btn.innerHTML = this.order;
-    }
-
-    /**
-     * Gets the order number of the button.
-     * @returns the order number of the button
-     */
-    getNumber(){
-        return this.order;
     }
 
     /**
@@ -457,19 +327,24 @@ class ScrambleButton extends Button{
  */
 class Display{
 
+    /**
+     * Creates a new Display element with a div for displaying items in. The div
+     * is appended to the top of the body element.
+     */
     constructor(){
         this.div = document.createElement("div");
-        this.div.id = "displayDiv";
-        this.message = document.createElement("p");
-        this.message.innerHTML = "Button Game";
-        this.message.className = "displayMessage";
-        this.div.appendChild(this.message);
+        //this.div.id = "displayDiv";
+        this.div.className = "spacing";
         document.body.appendChild(this.div);
     }
 
+    /**
+     * Creates a div with the buttonDiv class.
+     * @returns a div with the buttonDiv class
+     */
     static getButtonDisplayDiv(){
         let buttonDiv = document.createElement("div");
-        buttonDiv.className = "buttonDiv";
+        buttonDiv.className = "buttonDiv spacing";
         return buttonDiv;
     }
 
@@ -486,33 +361,30 @@ class Display{
         return colour;
     }
 
-    // // REMOVE LATER
-    // static getRandomHeight(){
-    //     return Math.floor(Math.random() * (window.innerHeight));
-    //     //return Math.floor(Math.random() * (screen.height));
-    // }
+    /**
+     * Displays a message in an alert for the user.
+     * @param {*} message 
+     */
+    static displayMessage(message){
+        alert(message);
+    }
 
-    // // REMOVE LATER
-    // static getRandomWidth(){
-    //     return Math.floor(Math.random() * (window.innerWidth));
-    //     //return Math.floor(Math.random() * (screen.width));
-    // }
-
+    /**
+     * Adds an element to the display div.
+     * @param {*} element 
+     */
     addElementToDiv(element){
         this.div.appendChild(element);
     }
 
     /**
      * Gets a random vertical location on the screen, taking into account the height of the element
-     * to ensure it does not go off the screen.
+     * to ensure it does not go off the screen and the height of the display div.
      * @param {*} height - the height of the element to be placed
      * @returns a random vertical location on the screen
      */
     getRandomLocationVertical(height){
-        // return (Math.floor(Math.random() * (window.innerHeight - height)) - this.div.offsetHeight);
-        console.log("DIV HEIGHT: " + this.div.offsetHeight);
         return (Math.floor(Math.random() * (window.innerHeight - height - this.div.offsetHeight)) + this.div.offsetHeight);
-        //return Math.floor(Math.random() * (screen.height));
     }
 
     /**
@@ -523,37 +395,9 @@ class Display{
      */
     getRandomLocationHorizontal(width){
         return Math.floor(Math.random() * (window.innerWidth - width));
-        //return Math.floor(Math.random() * (screen.width));
     }
-
-    displayMessage(message){
-        this.message.innerHTML = message;
-    }
-
-    // // REMOVE LATER
-    // static setButtonLocation(totalButtons, button){
-    //     let left = (screen.width - 50) / (totalButtons * (button.getNumber()));
-    //     let top = 50;
-    //     button.setLocation(left + "px", top + "px");
-
-    // }
 }
 
-// class GoButton extends Button{
-//     constructor(text, id, colur, width, height, onClickFunction){
-//         super(text, id, colur, width, height);
-//         this.btn.onclick = onClickFunction;
-//     }
-// }
 
-
-// TESTING PURPOSES
-
-function testStart(){
-    console.log("Go button clicked!");
-}
-function begin(){
-    new Game();
-}
-
-begin();
+// Creates a new game
+new Game();
